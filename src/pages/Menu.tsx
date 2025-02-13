66f1da56933299236b5962fca7ac5c691c6e32c9
vince072618 <vincentroycamba072618@gmail.com>
2025-02-13 21:28:15 +0800

    Added Files src/pages

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,29 @@
+
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Menu: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Menu;
