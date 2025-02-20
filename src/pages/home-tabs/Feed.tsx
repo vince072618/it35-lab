34702c5219ce464f97b6e8a4cf3ed4d04b756be4
vince072618 <vincentroycamba072618@gmail.com>
2025-02-20 13:26:17 +0800

    Added /home-tabs/pages

---
+++ src/pages/home-tabs/Feed.tsx
@@ -0,0 +1,28 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Feed: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Feed</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Feed;
