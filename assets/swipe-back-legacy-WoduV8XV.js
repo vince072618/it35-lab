4e7e703ff069f4ce8083292bbc27f649fe2619c6
vince072618 <vincentroycamba072618@gmail.com>
2025-03-03 14:55:38 +0800

    Updates

--- assets/swipe-back-legacy-WoduV8XV.js
+++ assets/swipe-back-legacy-WoduV8XV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-IMccuooh.js","./index3-legacy-SfjxHmIk.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Bxxwkyeb.js","./index3-legacy-BmALUzPK.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
