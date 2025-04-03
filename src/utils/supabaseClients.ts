366c1eb0e80c2babc83132448c07a1e1e47cc419
vince072618 <vincentroycamba072618@gmail.com>
2025-04-03 16:10:58 +0800

    added supabaseclients.ts url key and anon key

---
+++ src/utils/supabaseClients.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient('https://slxadmotlehyyawoayxh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNseGFkbW90bGVoeXlhd29heXhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2NTY1NDAsImV4cCI6MjA1OTIzMjU0MH0.SoOdNTyYio12PTbLKAS0wP1OWJNNlY5FyadUsXi_YOY');
