670548df97bdb2fe77bb7852079a8ebb8575474a
vince072618 <vincentroycamba072618@gmail.com>
2025-04-03 21:19:27 +0800

    Updates

--- assets/md.transition-legacy-CugeLWcB.js
+++ assets/md.transition-legacy-CugeLWcB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D_v_D6mJ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-PTBwi4ZV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
