2ea32d72a8eace0c00242558e5ee1bd98ee75fe4
jamlagz28 <jameslaag2003@gmail.com>
2025-04-21 13:48:54 +0800

    modify the feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,83 +1,40 @@
 import { 
   IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonMenuButton, 
-  IonPage, 
-  IonTitle, 
-  IonToolbar, 
-  IonCard, 
-  IonCardContent, 
-  IonCardHeader, 
-  IonCardTitle, 
-  IonItem, 
-  IonLabel 
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
 } from '@ionic/react';
+import FeedContainer from '../../components/FeedContainer';
 
 const Feed: React.FC = () => {
-return (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonButtons slot='start'>
-          <IonMenuButton></IonMenuButton>
-        </IonButtons>
-        <IonTitle>Feed</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      <div className="content-container">
-        <h2>Featured Gamers</h2>
-
-        {/* Featured Game Stream 1 */}
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Stream: Fortnite Live</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonItem>
-              <IonLabel>
-                <h3>Streamer: GamerX</h3>
-                <p>Join GamerX as they battle it out in Fortnite live! Watch and interact in real time.</p>
-              </IonLabel>
-            </IonItem>
-          </IonCardContent>
-        </IonCard>
-
-        {/* Featured Game Stream 2 */}
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Stream: Valorant Showdown</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonItem>
-              <IonLabel>
-                <h3>Streamer: ProPlayer99</h3>
-                <p>Watch ProPlayer99 dominate in this intense Valorant showdown.</p>
-              </IonLabel>
-            </IonItem>
-          </IonCardContent>
-        </IonCard>
-
-        {/* Upcoming Streams Section */}
-        <h2>Upcoming Streams</h2>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>Upcoming: League of Legends Tournament</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonItem>
-              <IonLabel>
-                <h3>Streamer: TeamElite</h3>
-                <p>Don’t miss the upcoming tournament stream from TeamElite in League of Legends!</p>
-              </IonLabel>
-            </IonItem>
-          </IonCardContent>
-        </IonCard>
-      </div>
-    </IonContent>
-  </IonPage>
-);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          Feed
+        </div>
+        <FeedContainer />
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Feed;
